0a36cbc625a5e3ce126d941af4c176c3bdf2f4ec
Lapwell <76802132+Lapwell@users.noreply.github.com>
2022-05-17 00:18:43 -0700

    COMPLETED:     -Window.     -Ball movement.     -Player platform.     -Player movement.     -Ball bouncing off player
    TO DO:
        -Proper ricochet.
        -Bricks.
        -Brick destruction by ball.
        -Sound effects.
        -Art.
    
    OPTIONAL:
        -Particle for brick destruction.
        -Power ups.

--- main.py
+++ main.py
@@ -3,17 +3,32 @@ import sys
 
 pygame.init()
 
+# Colours
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 RED = 64, 64, 255
+GREEN = 0, 128, 255
 
+# Pygame related
 WIDTH, HEIGHT = 800, 600
 ROOT = pygame.display.set_mode((WIDTH, HEIGHT))
-FPS = 60
+FPS = 30
 FONT = pygame.font.Font(None, 24)
 clock = pygame.time.Clock()
 
 
+# This class handles the player's platform
+class PlayerClass:
+    def __init__(self, posx, posy, vel, length, width):
+        self.posx = posx
+        self.posy = posy
+        self.vel = vel
+        self.length = length
+        self.width = width
+        self.rect = pygame.Rect(self.posx, self.posy, self.length, self.width)
+
+
+# This class handles the ball
 class BallClass:
     def __init__(self, posx, posy, vel, size):
         self.posx = posx
@@ -22,7 +37,12 @@ class BallClass:
         self.size = size
         self.rect = pygame.Rect(self.posx, self.posy, self.size, self.size)
 
+    def update_pos(self):
+        self.rect.x += self.vel
+        self.rect.y += self.vel
 
+
+# This is the template to spawn the bricks
 class BrickClass:
     def __init__(self, posx, posy, size):
         self.posx = posx
@@ -31,16 +51,26 @@ class BrickClass:
         self.rect = pygame.Rect(self.posx, self.posy, self.size, self.size)
 
 
-def update_root():
-    pygame.draw.circle(ROOT, WHITE, (ball.rect[0], ball.rect[1]), ball.size)
-    pygame.display.update()
-
-
 def check_events():
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and player.rect.x > 4:
+        player.rect.x -= player.vel
+    if keys[pygame.K_RIGHT] and player.rect.x < WIDTH - player.length - 4:
+        player.rect.x += player.vel
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+
+def update_root():
+    if ball.rect.colliderect(player.rect):
+        ball.vel *= -1
+    ball.update_pos()
+    ROOT.fill(BLACK)
+    pygame.draw.circle(ROOT, GREEN, (ball.rect[0], ball.rect[1]), ball.size)
+    pygame.draw.rect(ROOT, WHITE, player.rect)
+    pygame.display.update()
 
 
 def fps_counter():
@@ -57,5 +87,6 @@ def main():
 
 if __name__ == '__main__':
     ball = BallClass(WIDTH // 2, HEIGHT // 2, 4, 15)
+    player = PlayerClass(WIDTH - WIDTH / 2, HEIGHT - 50, 4, 164, 12)
     while True:
         main()
